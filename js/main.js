fc1ecb2b173eb1954a9ef406b655907098b1d1fd
acho <achoegbunu@gmail.com>
2018-08-19 15:52:03 -0700

    first commit

---
+++ js/main.js
@@ -0,0 +1,3 @@
+// Create an array of 5 strings. Make sure to capitalize some of the letters in your strings
+var myArray = ["LaUghinG", "CrYinG", "SLeePinG", "PLAying", "WalKiNg"];
+var myNewArray = [];
